5defd60b635d908ddf5480a7fbd5457b6bbb6ad4
gxt-temp <gxt385@student.bham.ac.uk>
2024-12-09 22:39:05 +0000

    Update app.js
    Rename the url-derived property to match below

--- hss-smart-rentals/app.js
+++ hss-smart-rentals/app.js
@@ -132,7 +132,7 @@ app.get('/propertydetail_device', (req, res) => {
 
 // Fetch property room details (specifically the number of available rooms)
 app.get('/propertydetail_room', (req, res) => {
-    const { landlord_id } = req.query;
+    const { location } = req.query;
 
     let query = `select property.location,property.price,property.bedrooms,property.bathrooms,COUNT(status) as available from property
                     LEFT JOIN room on property.property_id = room.property_id
